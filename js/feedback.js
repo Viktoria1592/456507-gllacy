3aa7a657d35bbbff376e1bbfab4a2a0b5df8ef2c
De-Mel <30821269+De-Mel@users.noreply.github.com>
2017-09-12 14:03:16 +0600

    Доработал замечания проверяющего

--- js/feedback.js
+++ js/feedback.js
@@ -1,45 +1,45 @@
 //Окно обратной связи
-		var feedback = document.querySelector('.feedback');
+var feedback = document.querySelector(".feedback");
+
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var username = popup.querySelector("[name=username]");
+var email = popup.querySelector("[name=e-mail]");
+
+var storage = localStorage.getItem("username");
+
+feedback.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	if (storage) {
+		username.value = storage;
+		e-mail.focus();
+	} else {
+		username.focus();
+	}
+});
 		
-		var popup = document.querySelector(".modal-feedback");
-		var close = popup.querySelector(".modal-close");
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	/*login.focus();*/
+});
 		
-		var form = popup.querySelector("form");
-		var name = popup.querySelector("[name=name]");
-		var email = popup.querySelector("[name=e-mail]");
+form.addEventListener("submit", function (evt) {
+	if (!username.value || !email.value) {
+		evt.preventDefault();
+		console.log("Нужно ввести имя и почту");
+	} else {
+		localStorage.setItem("name", username.value);
+	}
+});
 		
-		var storage = localStorage.getItem("name");
-		
-		feedback.addEventListener("click", function (evt) {
-			evt.preventDefault();
-			popup.classList.add("modal-show");
-			if (storage) {
-				name.value = storage;
-				email.focus();
-			} else {
-				name.focus();
-			}
-		});
-		
-		close.addEventListener("click", function (evt) {
-			evt.preventDefault();
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (popup.classList.contains("modal-show")) {
 			popup.classList.remove("modal-show");
-			name.focus();
-		});
-		
-		form.addEventListener("submit", function (evt) {
-			if (!name.value || !email.value) {
-				evt.preventDefault();
-				console.log("Нужно ввести имя и почту");
-			} else {
-				localStorage.setItem("name", name.value);
-			}
-		});
-		
-		window.addEventListener("keydown", function (evt) {
-		if (evt.keyCode === 27) {
-		  if (popup.classList.contains("modal-show")) {
-			popup.classList.remove("modal-show");
-			}
-		  }
-		});
+		}
+	}
+});
--- js/search.js
+++ js/search.js
@@ -1,16 +1,16 @@
 //Окно поиска
-		var search = document.querySelector(".search");
-		var searchPopup = document.querySelector(".modal-search-form");
-		  
-		search.addEventListener("click", function (evt) {
-			evt.preventDefault();
-			searchPopup.classList.add("modal-show");
-		});
-		
-		window.addEventListener("keydown", function (evt) {
-		if (evt.keyCode === 27) {
-			if (searchPopup.classList.contains("modal-show")) {
-				searchPopup.classList.remove("modal-show");
-				}
-			}
-		});
+var search = document.querySelector(".search");
+var searchPopup = document.querySelector(".modal-search-form");
+
+search.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	searchPopup.classList.add("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (searchPopup.classList.contains("modal-show")) {
+			searchPopup.classList.remove("modal-show");
+		}
+	}
+});
